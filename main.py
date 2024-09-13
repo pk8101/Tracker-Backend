56737ff00c0324df9a56f2e944a63d518410ce9d
pk8101 <pradeepvirat913@gmail.com>
2024-09-13 18:52:11 +0530

    middleware added in application to take request from frontend

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app=FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # React's frontend origin
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all HTTP methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allow all headers
+)
+
 
 @app.get("/")
 async def hello():
     return {"msg":"hello pradeep"}
-    

f99e107f0c3b0911028ae0892e43f718de16fb66
pk8101 <pradeepvirat913@gmail.com>
2024-09-19 00:57:21 +0530

    configuration for data base is done and also model file created

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from configs.database import engine, Base
 
 app=FastAPI()
 
+
+
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # React's frontend origin
+    allow_origins=["*"],  # all origins 
     allow_credentials=True,
     allow_methods=["*"],  # Allow all HTTP methods (GET, POST, etc.)
     allow_headers=["*"],  # Allow all headers
 )
+
+
+
+# Create all tables in the database
+Base.metadata.create_all(bind=engine)
 
 
 @app.get("/")
---
+++ utils/models.py
@@ -0,0 +1,5 @@
+from ..configs.database import Base
+from sqlalchemy import Column , Integer ,String
+
+# create models for db
+    
